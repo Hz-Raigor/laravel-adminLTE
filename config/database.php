5cc9a2c793fe8e10e8e35f86038439279aa7e416
Hz-Raigor <hz1991217@126.com>
2018-05-29 10:08:11 +0800

    修改数据配置

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '106.15.45.247'),
-            'port' => env('DB_PORT', '13306'),
-            'database' => env('DB_DATABASE', 'dispatching_test'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'frt3233Rt57'),
+            'host' => env('DB_HOST', env('DB_HOST')),
+            'port' => env('DB_PORT', env('DB_PORT')),
+            'database' => env('DB_DATABASE', env('DB_DATABASE')),
+            'username' => env('DB_USERNAME', env('DB_USERNAME')),
+            'password' => env('DB_PASSWORD', env('DB_PASSWORD')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
